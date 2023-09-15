8a91826596ea1d4437a57093efade4ca9b0d76e8
Kaveesha Weerasinghe <88468262+KaveeshaGW@users.noreply.github.com>
2023-09-16 01:07:50 +0530

    Create q5.scala

---
+++ q5.scala
@@ -0,0 +1,12 @@
+object q5 extends App{
+  def EasyTime(x: Int):Int={
+    return x*8;
+  }
+  
+  def TempoTime(x:Int):Int={
+    return x*7;
+  }
+
+    println("Total time : "+(EasyTime(4)+TempoTime(3)));
+
+}
